a4bd8ddf0b2243eab95537c0154e8a01c6f52470
Filip Jerga <jerga99@gmail.com>
2023-11-06 09:33:04 +0000

    More Variables

--- variables.py
+++ variables.py
@@ -1,13 +1,20 @@
 
+x = 20
 
-sum_result = 10 + 5
+res_1 = x + 5
+res_2 = x - 5
+res_3 = x * 5
+res_4 = x / 5
+res_5 = x ** 2
+
+res_6 = (res_2 + res_1) * res_4
+
+print(res_6)
+
+# print(res_1)
+# print(res_2)
+# print(res_3)
+# print(res_4)
+# print(res_5)
 
 
-sum_result
-sum_result
-sum_result
-sum_result
-sum_result
-sum_result
-sum_result
-sum_result

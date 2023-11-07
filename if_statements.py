1be659720863c4e9461c9d5ada9d0609d9325583
Filip Jerga <jerga99@gmail.com>
2023-11-07 08:07:45 +0000

    If Elif

--- if_statements.py
+++ if_statements.py
@@ -1,18 +1,36 @@
 
 
-num_1 = 10
+# num_1 = 10
+# num_2 = 10
+
+# result_1 = num_1 < num_2
+# result_2 = num_1 > num_2
+# result_3 = num_1 == num_2
+# result_4 = num_1 <= num_2 # small or equal
+# result_5 = num_1 >= num_2 # greater or equal
+
+# print(f'Result 1: {result_1}')
+# print(f'Result 2: {result_2}')
+# print(f'Result 3: {result_3}')
+# print(f'Result 4: {result_4}')
+# print(f'Result 5: {result_5}')
+
+num_1 = 20
 num_2 = 10
+num_3 = 10
 
-result_1 = num_1 < num_2
-result_2 = num_1 > num_2
-result_3 = num_1 == num_2
-result_4 = num_1 <= num_2 # small or equal
-result_5 = num_1 >= num_2 # greater or equal
+result = num_1 > num_2
 
-print(f'Result 1: {result_1}')
-print(f'Result 2: {result_2}')
-print(f'Result 3: {result_3}')
-print(f'Result 4: {result_4}')
-print(f'Result 5: {result_5}')
+# if result:
+#     print('Hello World!')
+# else:
+#     print('Goodbye!')
+
+if num_1 > num_2:
+    print('Do one thing')
+elif num_2 == num_3:
+    print('Do other thing')
+else:
+    print('Do last thing')
 
 

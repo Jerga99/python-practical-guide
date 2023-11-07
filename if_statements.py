fb7a77fbf8186d038a14fc538067cf28d65a533e
Filip Jerga <jerga99@gmail.com>
2023-11-07 08:37:03 +0000

    More Ifs

--- if_statements.py
+++ if_statements.py
@@ -15,28 +15,47 @@
 # print(f'Result 4: {result_4}')
 # print(f'Result 5: {result_5}')
 
+# num_1 = 20
+# num_2 = 10
+# num_3 = 10
+
+# result = num_1 > num_2
+
+# if num_1 > num_2: print('How are you?')
+    
+# print('Hi There!')
+# print('Hello There')
+
 num_1 = 20
 num_2 = 10
-num_3 = 10
+num_3 = 100
 
-result = num_1 > num_2
+if num_1 > num_2:
+    print('Do one thing')
+else:
+    print('Something!')
 
-if num_1 > num_2: print('How are you?')
-    
-print('Hi There!')
-print('Hello There')
+if num_2 < num_1:
+    print('Do other thing')
+elif num_1 == num_3:
+    print('Something else!')
+else:
+    print('Hola!')
 
+if num_3 < 100:
+    print('Num 3 < 100')
+else:
+    print('Do last thing')
 
-# if result:
-#     print('Hello World!')
-# else:
-#     print('Goodbye!')
-
-# if num_1 > num_2:
+# if num_1 < num_2:
 #     print('Do one thing')
 # elif num_2 == num_3:
 #     print('Do other thing')
+# elif num_3 < 1000:
+#     print('Num 3 < 100')
 # else:
 #     print('Do last thing')
+
+
 
 

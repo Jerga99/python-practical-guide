24723c542e49afc92b84d4c592327fe1fad68b62
Filip Jerga <jerga99@gmail.com>
2023-11-07 07:55:28 +0000

    Simple Comparisons

---
+++ if_statements.py
@@ -0,0 +1,18 @@
+
+
+num_1 = 10
+num_2 = 10
+
+result_1 = num_1 < num_2
+result_2 = num_1 > num_2
+result_3 = num_1 == num_2
+result_4 = num_1 <= num_2 # small or equal
+result_5 = num_1 >= num_2 # greater or equal
+
+print(f'Result 1: {result_1}')
+print(f'Result 2: {result_2}')
+print(f'Result 3: {result_3}')
+print(f'Result 4: {result_4}')
+print(f'Result 5: {result_5}')
+
+

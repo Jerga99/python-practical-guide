c878b056737db65c597c4dfe3cabde8d13b50d01
Filip Jerga <jerga99@gmail.com>
2023-11-08 08:25:20 +0000

    While loop

---
+++ while.py
@@ -0,0 +1,24 @@
+
+
+# counter = 0
+# while True:
+#     print(f'We gonna crush - {counter}')
+#     # counter = counter + 1
+#     counter += 1
+
+
+# counter = 0
+
+# while counter < 1000:
+#     print(f'We gonna crush - {counter}')
+#     # counter = counter + 1
+#     counter += 1
+
+
+counter = 0
+
+while True:
+    print(f'We gonna crush - {counter}')
+    counter += 1
+
+    if counter == 500: break

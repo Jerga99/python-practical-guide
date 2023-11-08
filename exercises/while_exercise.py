54877f99b0b4dabfa8b4e84ecdef433b4ca2ecc0
Filip Jerga <jerga99@gmail.com>
2023-11-08 09:11:13 +0000

    Calculator assignment

---
+++ exercises/while_exercise.py
@@ -0,0 +1,47 @@
+# Simple Calculator for "addition" "substraction" "multiplication" "division
+#                           +            -               *             /
+
+# Create a infinite While loop 
+# Inside of this loop ask a user to input the first number
+# Inside of this loop ask a user to input the second number
+
+# Inside of this loop ask a user to input one of four options: (+, -, *, /)
+#   if user inputs '+' then add numbers and print result
+#   if user inputs '-' then substract numbers and print result
+#   if user inputs '*' then multiply numbers and print result
+#   if user inputs '/' then divide numbers and print result
+#   if user inputs any other character or string then start the while loop over and print
+#     - 'Wrong Input! Available options are: (+, -, *, /)'
+
+#   Ask user to input 'q' to quit the application 
+#   if user inputs 'q' the break the while loop and print 'Application has been closed'
+
+
+while True:
+    x = input('Enter first number: ')
+    y = input('Enter second number: ')
+
+    x = float(x)
+    y = float(y)
+
+    operation = input('Choose Operation (+, -, *, /): ')
+
+    if operation == '+':
+        print(f'Result is: {x + y}')
+    elif operation == '-':
+        print(f'Result is: {x - y}')
+    elif operation == '*':
+        print(f'Result is: {x * y}')
+    elif operation == '/':
+        print(f'Result is: {x / y}')
+    else:
+        print('Wrong Input! Available options are (+, -, *, /)')
+        continue
+
+    q_key = input('Press (q) to quit the application, press any other key to continue: ')
+
+    if q_key == 'q':
+        print('Application has been closed!')
+        break
+
+
--- while.py
+++ while.py
@@ -41,26 +41,37 @@
 
 
 
-COUNTER_GOAL = 500
-counter = 0
+# COUNTER_GOAL = 500
+# counter = 0
 
-while True:
+# while True:
 
-    counter += 1
+#     counter += 1
 
-    if counter >= 100 and counter <= 400:
-        print(f'Skipping on number {counter}')   
-        continue
+#     if counter >= 100 and counter <= 400:
+#         print(f'Skipping on number {counter}')   
+#         continue
 
-    if counter % 2 == 0:
-        print(f'Number {counter} is even!')
-    else:
-        print(f'Number {counter} is odd!')
+#     if counter % 2 == 0:
+#         print(f'Number {counter} is even!')
+#     else:
+#         print(f'Number {counter} is odd!')
    
-    if counter == COUNTER_GOAL:
-        print(f'Counter reached goal {COUNTER_GOAL}. We are done')
-        break
+#     if counter == COUNTER_GOAL:
+#         print(f'Counter reached goal {COUNTER_GOAL}. We are done')
+#         break
 
+
+
+# while True:
+#     user_name = input('Enter username: ')
+
+#     print(f'Hello {user_name}')
+
+#     q_key = input('Press q to quit the app: ')
+
+#     if q_key == 'q':
+#         break
 
 
     

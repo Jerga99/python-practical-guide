846aa926249b704fa58db4e71bd7e81e1616dd03
Filip Jerga <jerga99@gmail.com>
2023-11-09 08:25:26 +0000

    Define Function

---
+++ functions.py
@@ -0,0 +1,26 @@
+
+
+
+name = 'Filip'
+age = 32
+
+name_2 = 'Lucy'
+age_2 = 29
+
+# print(f'{name} is {age} years old')
+# print(f'{name_2} is {age_2} years old')
+
+def print_user_info():
+    print(f'{name} is {age} years old')
+
+    a = 2 + 2
+
+    if a == 4:
+        print(f'a is {a}')
+
+
+print_user_info()
+print_user_info()
+print_user_info()
+print_user_info()
+print_user_info()

8ddc1c9ef9deddbb5ed2e4827c9cc5d192c4de2f
Filip Jerga <jerga99@gmail.com>
2023-11-09 09:04:24 +0000

    More about Functions

--- functions.py
+++ functions.py
@@ -10,22 +10,24 @@ age_2 = 29
 # print(f'{name} is {age} years old')
 # print(f'{name_2} is {age_2} years old')
 
-def print_user_info(name, age):
-    print(f'{name} is {age} years old')
+def print_user_info(name, age, age_in_days = False):
+    age_measure = 'years'
 
-    a = 2 + 2
+    if age_in_days:
+        age_measure = 'days'
+        age *= 365
 
-    if a == 4:
-        print(f'a is {a}')
+    print(f'{name} is {age} {age_measure} old')
 
 
-print_user_info('Mark', 45)
+print_user_info('Mark', 45, True)
 print_user_info('John', 200)
-print_user_info('Alena', 23)
-print_user_info('Jess', 25)
+
+print_user_info('Alena', 23, True)
+print_user_info('Jess', 25, False)
 
 print_user_info(name, age)
-print_user_info(name_2, age_2)
+print_user_info(name_2, age_2, True)
 
 
 

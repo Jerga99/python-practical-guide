6242b33fa527c9a32ea5f4fc0886cf36f2750cea
Filip Jerga <jerga99@gmail.com>
2023-11-10 08:25:41 +0000

    Function returns

---
+++ function_return.py
@@ -0,0 +1,20 @@
+
+
+def add(num_1, num_2):
+    result = num_1 + num_2
+    return result
+
+result = add(10, 20)
+print(result)
+
+
+def welcome(name):
+    msg = f'Welcome {name}'
+    return msg
+
+
+print(welcome('Filip'))
+print(welcome('Jess'))
+
+
+

ae32958868278aa82682e3acc39755c68d926cd8
Filip Jerga <jerga99@gmail.com>
2023-11-10 10:55:13 +0000

    Fibonacci assignment

---
+++ exercises/fibonacci.py
@@ -0,0 +1,23 @@
+
+# This is harder assignment, you might check the internet
+# Think about solution for few moments
+# Note: you can use 'while' loop
+# if you are not sure about solution search for 'pseudo code fibonacci sequence'
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144.
+
+
+# 0 1 1 2 3 5 8 13 21 34 55 89 144 ...
+
+# Create a function to generate fibonacci sequence
+
+# if user call fibonacci(5) it should generate '0 1 | 1 2 3 5 8'
+# if user call fibonacci(7) it should generate '0 1 | 1 2 3 5 8 13 21'
+
+def fibonacci(length = 2):
+    pass
+
+
+
+fibonacci(5) # '0 1 1 2 3 5 8'
+fibonacci(7) # '0 1 1 2 3 5 8 13 21'

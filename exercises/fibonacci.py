9c8f8ba347ac5f9934ef21cfb5968f9b5ce4706b
Filip Jerga <jerga99@gmail.com>
2023-11-10 11:06:47 +0000

    Fibonacci solution

--- exercises/fibonacci.py
+++ exercises/fibonacci.py
@@ -15,9 +15,30 @@
 # if user call fibonacci(7) it should generate '0 1 | 1 2 3 5 8 13 21'
 
 def fibonacci(length = 2):
-    pass
+    if length < 0:
+        print('Length must be greater than 0!')
+        return
 
+    num_1 = 0
+    num_2 = 1
+    sequence = '0 1 | '
+    i = 0
 
+    while i < length:
+        result = num_1 + num_2
+        sequence += f'{result} '
+        num_1 = num_2
+        num_2 = result
+        i += 1
 
-fibonacci(5) # '0 1 1 2 3 5 8'
-fibonacci(7) # '0 1 1 2 3 5 8 13 21'
+    return sequence
+
+print(fibonacci(0))
+print(fibonacci(1))
+print(fibonacci())
+print(fibonacci(5))
+print(fibonacci(6))
+print(fibonacci(7))
+print(fibonacci(8))
+print(fibonacci(9))
+print(fibonacci(10))

a5b5e05cf2745437fb8db23398f02b317dc940e4
Filip Jerga <jerga99@gmail.com>
2023-11-10 08:40:17 +0000

    Define data types

--- function_return.py
+++ function_return.py
@@ -18,3 +18,20 @@ print(welcome('Jess'))
 
 
 
+def nice_function(x: int, y: float, print_message: str, should_increment = True) -> float:
+    result = x + y
+
+    if should_increment:
+        result *= 10
+
+    print(f'{print_message}{result}')
+
+    return result
+
+
+result = nice_function(10, 2.82, 'Result is: ')
+print(result)
+
+
+
+

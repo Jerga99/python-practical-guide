11471fdd337f4ef81fc574d6181ce97f41543530
Filip Jerga <jerga99@gmail.com>
2023-11-11 21:19:28 +0000

    More list operations

--- lists.py
+++ lists.py
@@ -21,16 +21,58 @@ name_2 = 'James'
 # print(mix[-1])
 # print(mix[-4])
 
-names: list[str] = ['Filip', 'James', 'Ema', 'Jess', 'David']
+# names: list[str] = ['Filip', 'James', 'Ema', 'Jess', 'David']
+# new_names = names[1:4]
 
-print(names[1:4])
-print(names[:4])
-print(names[1:])
-print(names[-4:-1])
+# print(names[:4])
+# print(names[1:])
+# print(names[-4:-1])
 
-searched_name = input('Please enter a name: ')
+# searched_name = input('Please enter a name: ')
 
-if searched_name in names:
-    print(f'Name {searched_name} is in the list!')
-else:
-    print(f'{searched_name} Not in the list!')
+# if searched_name in names:
+#     print(f'Name {searched_name} is in the list!')
+# else:
+#     print(f'{searched_name} Not in the list!')
+
+
+numbers = [1,3,2,10,9,11]
+i = 0
+
+while i < len(numbers):
+    print(numbers[i])
+    i+=1
+
+# TODO: Find index of number 10 in the 'numbers' list and print it out
+
+
+numbers = [1,3,2,10,9,11]
+i = 0
+
+while i < len(numbers):
+    if numbers[i] == 10:
+        print(f'Number {numbers[i]} found on index {i}')
+    i+=1
+
+
+import random
+
+# numbers = [1,3,2,10,9,11]
+# new_numbers =[]
+# i = 0
+
+# while i < len(numbers):
+#     new_numbers.append(random.randint(900, 999))
+#     i+=1
+
+# print(new_numbers)
+
+numbers = [1,3,2,10,9,11]
+i = 0
+
+while i < len(numbers):
+    if i % 2 == 0:
+        numbers[i] = random.randint(900, 999)
+    i+=1
+
+print(numbers)

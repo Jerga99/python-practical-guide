ab429110a5fef56dad215d93c8c946a084d79e88
Filip Jerga <jerga99@gmail.com>
2023-11-11 21:39:11 +0000

    Remove item from list

--- lists.py
+++ lists.py
@@ -36,26 +36,26 @@ name_2 = 'James'
 #     print(f'{searched_name} Not in the list!')
 
 
-numbers = [1,3,2,10,9,11]
-i = 0
+# numbers = [1,3,2,10,9,11]
+# i = 0
 
-while i < len(numbers):
-    print(numbers[i])
-    i+=1
+# while i < len(numbers):
+#     print(numbers[i])
+#     i+=1
 
 # TODO: Find index of number 10 in the 'numbers' list and print it out
 
 
-numbers = [1,3,2,10,9,11]
-i = 0
+# numbers = [1,3,2,10,9,11]
+# i = 0
 
-while i < len(numbers):
-    if numbers[i] == 10:
-        print(f'Number {numbers[i]} found on index {i}')
-    i+=1
+# while i < len(numbers):
+#     if numbers[i] == 10:
+#         print(f'Number {numbers[i]} found on index {i}')
+#     i+=1
 
 
-import random
+# import random
 
 # numbers = [1,3,2,10,9,11]
 # new_numbers =[]
@@ -67,12 +67,50 @@ import random
 
 # print(new_numbers)
 
+# numbers = [1,3,2,10,9,11]
+# i = 0
+
+# while i < len(numbers):
+#     if i % 2 == 0:
+#         numbers[i] = random.randint(900, 999)
+#     i+=1
+
+# print(numbers)
+
+
+# names:list[str] = []
+
+# names.append('John')
+# names.append('Jess')
+# names.append('David')
+
+# print(names)
+# names.pop(0)
+# print(names)
+# names.pop(1)
+# print(names)
+
+
+# names = ['John', 'Jess', 'David', 'Ema']
+
+# found_name = names[50]
+
+# names.append('Rob')
+
+# deleted_name = names.pop(1)
+# print(f'I have deleted {deleted_name}')
+
+# del names[-1]
+
+import random
+
 numbers = [1,3,2,10,9,11]
 i = 0
 
 while i < len(numbers):
     if i % 2 == 0:
-        numbers[i] = random.randint(900, 999)
+        numbers.pop(i)
     i+=1
 
 print(numbers)
+

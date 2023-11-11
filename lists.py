826a87361cefad34da6dc326b30aa19d6a6442e8
Filip Jerga <jerga99@gmail.com>
2023-11-11 20:40:15 +0000

    Lists

---
+++ lists.py
@@ -0,0 +1,22 @@
+
+name_1 = 'Filip'
+name_2 = 'James'
+
+names: list[str] = ['Filip', 'James', 'Ema', 'Jess', 'David']
+
+print(names)
+fruits = list(('apple', 'banana', 'watermelon'))
+print(fruits)
+mix = ['apple', 1, True, 3.14]
+print(mix)
+
+
+print(names[2])
+
+name = names[2]
+
+print(len(names))
+
+print(names[1] + names[3])
+print(mix[-1])
+print(mix[-4])

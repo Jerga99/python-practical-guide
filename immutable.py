04b355c50eccc74efce5d5f5be60146a7b673a85
Filip Jerga <jerga99@gmail.com>
2023-11-12 20:37:15 +0000

    Background to variables

--- immutable.py
+++ immutable.py
@@ -2,38 +2,61 @@
 # str, float, int, bool -> immutable -> cannot be changed
 # list -> mutable -> can be changed
 
-a = 'Filip'
-x = a.upper()
+# a = 'Filip'
+# x = a.upper()
 
-print(f'a: {hex(id(a))}')
-print(f'x: {hex(id(x))}')
+# print(f'a: {hex(id(a))}')
+# print(f'x: {hex(id(x))}')
 
-print(a)
-print(x)
+# print(a)
+# print(x)
 
-print('----------------------')
-print('----------------------')
+# print('----------------------')
+# print('----------------------')
 
-b = 30
-y = b
-y += 30
+# b = 30
+# y = b
+# y += 30
 
-print(f'b: {hex(id(b))}')
-print(f'y: {hex(id(y))}')
+# print(f'b: {hex(id(b))}')
+# print(f'y: {hex(id(y))}')
 
-print(b)
-print(y)
+# print(b)
+# print(y)
 
-print('----------------------')
-print('----------------------')
+# print('----------------------')
+# print('----------------------')
 
-f = [1,2,3]
-e = f
-e.append(12)
+# f = [1,2,3]
+# e = f
+# e.append(12)
 
-print(f'f: {hex(id(f))}')
-print(f'e: {hex(id(e))}')
+# print(f'f: {hex(id(f))}')
+# print(f'e: {hex(id(e))}')
+
+# print(f)
+# print(e)
+
+# print(bin(ord(a[0])))
+# print(bin(ord(a[1])))
+# print(bin(ord(a[2])))
+# print(bin(ord(a[3])))
+# print(bin(ord(a[4])))
+
+# import ctypes
+
+# a = 100
+# b = 'Filip Jerga'
+# c = 3.14
+
+# print(f'a: {(id(a))}')
+# print(f'b: {(id(b))}')
+# print(f'c: {(id(c))}')
+
+# while True:
+#     hex_address = input('Enter decimal address: ')
+#     value = ctypes.cast(int(hex_address), ctypes.py_object).value
+#     print(f'Value is: {value}')
 
 
-print(f)
-print(e)
+

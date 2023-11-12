2675dce70747be7dda098278b2b31cecbf0c6a07
Filip Jerga <jerga99@gmail.com>
2023-11-12 20:44:55 +0000

    User list assignment

---
+++ exercises/user_list.py
@@ -0,0 +1,39 @@
+# 1. Create list of user names, with initial values ['John', 'Peter']
+# 2. Create 4 functions - 
+#       - add_user -> adds new name to the user list
+#       - remove_user -> removes a name from the user list
+#       - move_user -> move a user from one position to another in the list
+#       - print_users -> print all users in the list
+
+# 3. Create Infinite While Loop
+# 4. Inside of the while loop ask user to input 4 options
+#       - 'a' -> to add user
+#       - 'r' -> to remove user
+#       - 'm' -> to move user
+#       - 'p' -> to print user list
+
+# 5. After selection a specific option ask user to provide:
+#    'name' (if add or remove was selected), and add or remove user
+#    'from_idx', 'to_idx' (if move user was select), also display list after operation
+
+# 6. If user inputs 'q' then quit the while loop
+
+# Note: you don't need to implement any safe checks, just focus on functionality
+
+
+all_users = ['John', 'Peter']
+
+def add_user(name: str):
+    pass
+
+def remove_user(name: str):
+    pass
+
+def move_user(from_idx: int, to_idx: int):
+    pass
+
+def print_users():
+    pass
+
+while True:
+    pass

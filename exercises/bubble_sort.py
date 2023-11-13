dea8f99d31fd7efe4d9e6207d20a85d18d217524
Filip Jerga <jerga99@gmail.com>
2023-11-13 17:41:40 +0000

    Optimize sort

--- exercises/bubble_sort.py
+++ exercises/bubble_sort.py
@@ -2,9 +2,11 @@
 
 # [1,2,3,4,5] -> bubble_sort -> [5,4,3,2,1]
 
-def bubble_sort(items: list) -> list:
+def bubble_sort(items: list, optimize = False) -> list:
+    items_count = 0
     for _ in items[1:]:
         i = 0
+        is_sorted = False
         while i < len(items) - 1:
             num_1 = items[i]
             num_2 = items[i + 1]
@@ -12,20 +14,34 @@ def bubble_sort(items: list) -> list:
             if num_1 < num_2:
                 items[i] = num_2
                 items[i + 1] = num_1
+                is_sorted = True
 
             i+=1
+            items_count += 1
+
+        if not is_sorted and optimize:
+            break
+
+    print(f'Items count: {items_count}')
     return items
 
-
 numbers = [2, 3, 5, 10, 2, 1, 14, 15, 12, 4, 20, 8]
+numbers_o = [2, 3, 5, 10, 2, 1, 14, 15, 12, 4, 20, 8]
+
 numbers_2 = [47, 92, 33, 74, 86, 51, 62, 90, 19, 58, 20, 64, 55, 91, 89, 66, 60, 23, 80, 7]
+numbers_2_o = [47, 92, 33, 74, 86, 51, 62, 90, 19, 58, 20, 64, 55, 91, 89, 66, 60, 23, 80, 7]
+
 numbers_3 = [39, 70, 75, 56, 98, 67, 20, 23, 47, 54, 89, 60, 42, 58, 83, 22, 77, 73, 85, 45]
-numbers_4 = [31.03515879799672, 36.91140541273432, 55.11915972252316, 95.39362918961595, 75.92365533469476, 96.01254852635835, 95.45670229074851, 72.68787248729374, 66.40651467564225, 53.62927666298624, 13.936512420079107, 49.69701942691797, 20.151722005848505, 36.24977491904222, 89.78416927019383, 34.76057032205579, 36.9686646116144, 57.05862243079372, 34.675747064294946, 15.462264741740108]
-numbers_5 = [-18, 27, -50, 10, 45, -38, -6, -33, 19, 50, 15, -35, 23, 36, 37, 38, 44, 16, -26, -8]
+numbers_3_o = [39, 70, 75, 56, 98, 67, 20, 23, 47, 54, 89, 60, 42, 58, 83, 22, 77, 73, 85, 45]
 
 print(bubble_sort(numbers))
+print(bubble_sort(numbers_o, optimize=True))
+
 print(bubble_sort(numbers_2))
+print(bubble_sort(numbers_2_o, optimize=True))
+
 print(bubble_sort(numbers_3))
-print(bubble_sort(numbers_4))
-print(bubble_sort(numbers_5))
+print(bubble_sort(numbers_3_o, optimize=True))
+
+
 

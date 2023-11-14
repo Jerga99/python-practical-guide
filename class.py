db8120e998fd28d607927d18ab30e5d45aa22d43
Filip Jerga <jerga99@gmail.com>
2023-11-14 08:45:51 +0000

    Method improvements

--- class.py
+++ class.py
@@ -6,8 +6,13 @@ class Person:
         self.age = 100
         self.notes = list[str]()
 
-    def add_note(self, note: str):
+    def add_note(self, note: str) -> 'Person':
         self.notes.append(note)
+        return self
+
+    def add_notes(self, *notes: str):
+        for note in notes:
+            self.notes.append(note)
 
     def remove_note(self, index: int):
         self.notes.pop(index)
@@ -20,9 +25,11 @@ class Person:
 person_1 = Person('Filip')
 person_2 = Person('John')
 
-person_1.add_note('Buy milk')
+person_1.add_note('Buy milk').add_note('Heya').add_note('Whatever')
 person_1.add_note('Learn Python')
 person_1.add_note('Go outside')
+
+person_1.add_notes('Note 1', 'Note 2', 'Note 3')
 
 person_1.remove_note(0)
 
@@ -35,6 +42,3 @@ person_2.add_note('Go inside')
 person_2.remove_note(-1)
 
 person_2.print_notes()
-
-
-

a78aa1cec8a2c1a839c3b3dfbd6be2d7e87a7133
Filip Jerga <jerga99@gmail.com>
2023-11-15 08:58:15 +0000

    Shape assignment

---
+++ exercises/shapes.py
@@ -0,0 +1,25 @@
+# Objective: Create classes for different geometric shapes where
+# specific shapes inherit from a base class Shape.
+
+# Base Shape Class:
+# Create a base class Shape with general attributes and methods common to all shapes,
+# such as __str__, area and perimeter calculations
+
+# Inheritance for Specific Shapes:
+# Implement derived classes Rectangle, Square and Circle that
+# inherit from the base class Shape, each defining methods to calculate
+# its specific area and perimeter.
+
+# c = Circle(3)
+# s = Square(5)
+# We need two values for both sides
+# r = Rectangle(2, 22)
+
+# shapes: tuple[Shape] = (c,s,r)
+
+# for shape in shapes: print(shape)
+
+# print should ouput:
+#   Area of Circle is '...', Perimeter of Circle is: '...'
+#   Area of Square is '...', Perimeter of Square is: '...'
+#   Area of Rectangle is '...', Perimeter of Rectangle is: '...'
